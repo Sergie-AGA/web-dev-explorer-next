f12919f9abc9134f9733f28fd4d502d5ef6a9289
Sergie <sergiearruda@gmail.com>
2024-09-25 15:56:42 +0100

    Minor adjustments and bug fixing

--- src/components/Global/GlobalHeader/GlobalHeader.tsx
+++ src/components/Global/GlobalHeader/GlobalHeader.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 interface IHeaderProps {
@@ -10,13 +11,15 @@ export default function GlobalHeader({ title, children }: IHeaderProps) {
   return (
     <header className="max-width-container flex items-center mb-4 md:mb-6">
       <div className="flex items-center mr-2">
-        <Image
-          src="/Logo.svg"
-          width={48}
-          height={48}
-          alt="Project Logo"
-          className="w-12 h-12 mr-3"
-        />
+        <Link href="/">
+          <Image
+            src="/Logo.svg"
+            width={48}
+            height={48}
+            alt="Project Logo"
+            className="w-12 h-12 mr-3"
+          />
+        </Link>
         <h1 className="h4">{title ? title : "Web Dev Explorer"}</h1>
       </div>
       <div className="ml-auto">{children}</div>
