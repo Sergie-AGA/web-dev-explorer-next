939b2ffe8bc63478cac67805eed4a5007e8c2fb6
Sergie <sergiearruda@gmail.com>
2024-11-04 15:23:44 +0000

    Further Cypress E2E tests for the project World of CSS

--- cypress/e2e/world-of-css.cy.ts
+++ cypress/e2e/world-of-css.cy.ts
@@ -1,16 +1,29 @@
 describe("World of CSS Page", () => {
+  let inputTestString = "Lorem Ipsum";
+
   beforeEach(() => {
     cy.visit("/world-of-css-effects");
   });
 
   it("Changes the current active section", () => {
-    // Get current effect
-    // Get current sidebar
-    // Click on item, show new sidebar
-    // Old sidebar not on screen
-    // Click on effect, rerender new component
-    // Old effect no on screen
-    // Show active class on clicked item
+    cy.get('[data-testid="sidebar-neon"]').then(($el) => {
+      if (!$el.hasClass("pointer-events-none")) {
+        cy.wrap($el).click();
+      }
+    });
+    cy.contains("This is the neon text effect");
+    cy.get('[data-testid="sidebar-glitch"]').should("exist");
+
+    cy.get('[data-testid="sidebar-background"]').then(($el) => {
+      if (!$el.hasClass("pointer-events-none")) {
+        cy.wrap($el).click().should("have.class", "pointer-events-none");
+      }
+    });
+    cy.get('[data-testid="sidebar-glitch"]').should("not.be.visible");
+    cy.get('[data-testid="sidebar-bIcons"]')
+      .click()
+      .should("have.class", "pointer-events-none");
+    cy.get(".neonText").should("not.exist");
   });
 
   it("Handles the Glitch effect correctly", () => {
@@ -20,39 +33,97 @@ describe("World of CSS Page", () => {
       }
     });
     cy.get('[data-testid="iconX"]').click();
-    cy.get("input").clear().type("My test");
+    cy.get("input").clear().type(inputTestString);
     cy.get("div.glitch-container").each(($div) => {
       cy.wrap($div).find("span").should("have.length", 3);
     });
   });
-  it.only("Handles the Neon effect correctly", () => {
+  it("Handles the Neon effect correctly", () => {
     cy.get('[data-testid="sidebar-neon"]').then(($el) => {
       if (!$el.hasClass("pointer-events-none")) {
         cy.wrap($el).click();
       }
     });
     cy.get('[data-testid="iconX"]').click();
-    cy.get("input").clear().type("My test");
+    cy.get("input").clear().type(inputTestString);
     cy.get('button[role="switch"]').click();
     cy.get(".neonText")
       .should("have.class", "neon-flicker")
-      .and("have.text", "My test");
+      .and("have.text", inputTestString);
   });
   it("Handles the Switching effect correctly", () => {
-    // Click on Switching effect
-    // Add new text
-    // Verify SwitchLetter component is rendered with specific class. Verify how many messages
-    // Remove text and verify how many messages
+    cy.get('[data-testid="sidebar-switch"]').then(($el) => {
+      if (!$el.hasClass("pointer-events-none")) {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get('[data-testid="iconX"]').click();
+
+    cy.get("input").type(inputTestString);
+    cy.get('[data-testid="switch-text-button"]').click();
+
+    cy.get('[data-testid="switching-text-list"]')
+      .find("li")
+      .should("have.length", 3)
+      .last()
+      .as("thirdListItem");
+
+    cy.get("@thirdListItem").find(".cursor-pointer").click();
+
+    cy.get('[data-testid="switching-text-list"]')
+      .find("li")
+      .should("have.length", 2);
   });
   it("Handles the Random Colour effect correctly", () => {
-    // Click on Random Colour effect
-    // Verify RandomColourLetter component is rendered with specific class
-    // Hover letters and check modified style exists
+    cy.get('[data-testid="sidebar-ranCol"]').then(($el) => {
+      if (!$el.hasClass("pointer-events-none")) {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get('[data-testid="iconX"]').click();
+    cy.get("input").clear().type(inputTestString);
+
+    cy.get('[data-testid="random-colour"]')
+      .find("span")
+      .each(($span) => {
+        cy.wrap($span).should("not.contain", "color");
+        cy.wrap($span).trigger("mouseover");
+        cy.wrap($span).should("have.attr", "style").and("include", "color");
+      });
   });
   it("Handles the Moving Text effect correctly", () => {
-    // Click on Moving Text effect
-    // Verify MovingLetter component is rendered with specific class
-    // Click on letters and check they move
+    cy.get('[data-testid="sidebar-move"]').then(($el) => {
+      if (!$el.hasClass("pointer-events-none")) {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get('[data-testid="iconX"]').click();
+    cy.get("input").clear().type(inputTestString);
+
+    cy.get('[data-testid="moving-text"]')
+      .find("span")
+      .each(($span) => {
+        cy.wrap($span).should(
+          "have.css",
+          "transform",
+          "matrix(1, 0, 0, 1, 0, 0)"
+        );
+      })
+      .then(($spans) => {
+        $spans.each((index, span) => {
+          cy.wrap(span)
+            .trigger("mouseover")
+            .should("not.have.css", "transform", "matrix(1, 0, 0, 1, 0, 0)");
+        });
+        cy.wait(2000);
+        $spans.each((index, span) => {
+          cy.wrap(span).should(
+            "have.css",
+            "transform",
+            "matrix(1, 0, 0, 1, 0, 0)"
+          );
+        });
+      });
   });
   it("Handles the Icon Background Generation effect correctly", () => {
     // Click on Icon Background Generation effect
@@ -66,9 +137,31 @@ describe("World of CSS Page", () => {
     // Verify items are disappearing
     // Toggle checkbox, modify slider size and verify size changed
   });
-  it("Handles the CSS Phone effect correctly", () => {
-    // Click on CSS Phone effect
-    // Send message as user, verify it rendered in the right spot
-    // Send message as bot, verify it rendered in the right spot
+  it.only("Handles the CSS Phone effect correctly", () => {
+    cy.get('[data-testid="sidebar-art"]').then(($el) => {
+      if (!$el.hasClass("pointer-events-none")) {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get('[data-testid="sidebar-phone"]').then(($el) => {
+      if (!$el.hasClass("pointer-events-none")) {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get('[data-testid="iconX"]').click();
+
+    cy.get('[data-testid="user-input"]').type(inputTestString);
+    cy.get('[data-testid="user-button"]').click();
+    cy.get("#screen")
+      .children()
+      .first()
+      .should("contain.text", inputTestString);
+
+    cy.get('[data-testid="bot-input"]').type(inputTestString + "2");
+    cy.get('[data-testid="bot-button"]').click();
+    cy.get("#screen")
+      .children()
+      .eq(1)
+      .should("contain.text", inputTestString + "2");
   });
 });
